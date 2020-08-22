e2262555686d60f3b6baf7733ef0a213e8041489
Brent McDowell <git@theceleritas.com>
2020-08-22 17:44:53 +1200

    Queue updates.

--- internal/worker/consumer.go
+++ internal/worker/consumer.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (a *App) Start() {
-	a.Logger.Info().Msgf("listening for messages on %v....", os.Getenv("CLOUDAMQP_QUEUE_DATA"))
+	a.Logger.Info().Msgf("listening for messages on %v....", a.queue())
 
 	conn, err := amqp.Dial(os.Getenv("CLOUDAMQP_URL"))
 	if err != nil {
@@ -32,12 +32,16 @@ func (a *App) Start() {
 	go a.run(msgs)
 	go func() {
 		<-a.Signalling.quit
-		a.Logger.Info().Msgf("stop listening on %v....", os.Getenv("CLOUDAMQP_QUEUE_DATA"))
+		a.Logger.Info().Msgf("stop listening on %v....", a.queue())
 		a.Signalling.cancel()
 		_ = conn.Close()
 		_ = ch.Close()
 	}()
 	<-a.Signalling.stop
+}
+
+func (a *App) queue() string {
+	return os.Getenv("CLOUDAMQP_QUEUE_REPORTS_PUBLISHED")
 }
 
 func (a *App) run(msgs <-chan amqp.Delivery) {
@@ -55,24 +59,9 @@ func (a *App) run(msgs <-chan amqp.Delivery) {
 }
 
 func (a *App) declare(ch *amqp.Channel) (amqp.Queue, error) {
-	return ch.QueueDeclare(
-		os.Getenv("CLOUDAMQP_QUEUE_DATA"),
-		true,
-		true,
-		false,
-		false,
-		nil,
-	)
+	return ch.QueueDeclare(a.queue(), true, true, false, false, nil)
 }
 
 func (a *App) consume(ch *amqp.Channel, q amqp.Queue) (<-chan amqp.Delivery, error) {
-	return ch.Consume(
-		q.Name,
-		"",
-		true,
-		false,
-		false,
-		false,
-		nil,
-	)
+	return ch.Consume(q.Name, "", true, false, false, false, nil)
 }
